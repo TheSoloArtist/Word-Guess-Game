28f8105261a57b9c529b3013ec708cb0d40a7de7
The Solo Artist <michael.solorzano@email.bakersfieldcollege.edu>
2020-04-25 23:41:49 -0700

    changed file name to reflect my file path+newfiles

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,1 +1,33 @@
-alert("TeST");
+
+
+// global variables
+
+var letterOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+var selectedLetter = "";
+var numBlanks = 0;
+var blanksAndSuccesses = []; 
+
+// counters
+var winCount = 0;
+var lossCunt = 0;
+var guessesLeft = 0;
+// Functions
+function startGame () {
+    selectedLetter = letterOptions [Math.floor(Math.random() * letterOptions.length)];
+    letters = selectedLetter.length;
+
+
+    //test/debug
+    console.log(selectedLetter);
+    console.log(letters);
+    console.log
+    console.log
+    console.log
+}
+
+
+
+
+// Main process
+startGame();
+
